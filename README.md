147cf8683108516848a4db1365bf85987a72a8e1
paritashah14 <paritashah14@yahoo.com>
2017-06-27 23:38:41 -0400

    works

--- README.md
+++ README.md
@@ -1,28 +1,2 @@
 # Bamazon
-This is an Amazon-like storefront with MySQL. The app will  welcome customers to place  in orders and display stock from the store's inventory.
-
-#1: Customer View
-
-Running this application will first display all of the items available for sale. Include the IDs, names, and prices of products for sale.
-
-The app will prompt users with two questions.
-
-The first question will ask the user the ID of the product they would like to buy.
-The second question will ask how many units of the product the user would like to buy
-
-screenshot-1
-
-
-
-
-
-
-Once the customer has placed the order, this application will check if your store has enough of the product to meet the customer's request.
-
-If not, the app will log a phrase like Sorry, insufficent in stock!, and then prevent the order from going through.
-However, if my store does have enough of the product, it will fulfill the customer's order.
-This will update the SQL database to reflect the remaining quantity.
-Once the update goes through, it will show the customer the total cost of their purchase.
-
-screenshot-2
-
+Bamazon is a store like app where customer can place order 
--- bamazoncustomer.js
+++ bamazoncustomer.js
@@ -1,6 +1,7 @@
 var mysql = require('mysql');
 var inquirer = require('inquirer');
-
+var colors = require('colors');
+//creates a connection to sql
 var connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
@@ -13,3 +14,105 @@ connection.connect(function(err) {
     if (err) throw err;
     console.log("connected as id " + connection.threadId);
 });
+connection.query("SELECT * FROM products", function(err, res) {
+
+    console.log('======================Welcome to BAMAZON============================='.red)
+    console.log("");
+    console.log("");
+
+    for (var i = 0; i < res.length; i++) {
+        console.log("ID: " + res[i].ItemID + " | " + "Product: " + res[i].ProductName + " | " + "Department: " + res[i].DepartmentName + " | " + "Price: " + res[i].Price + " | " + "QTY: " + res[i].StockQuantity);
+        console.log('--------------------------------------------------------------------------------------------------')
+    }
+    console.log("");
+    start();
+});
+
+
+
+var start = function() {
+
+    inquirer.prompt([{
+            name: "id",
+            type: "input",
+            message: "Which ID of the product would you like to buy?".green,
+            validate: function(value) {
+                if (isNaN(value) === false && value <= 10) {
+                    return true;
+                } else {
+                    return false;
+                }
+            }
+        }, {
+            name: "quantity",
+            type: "input",
+            message: "How much would you like to purchase?".blue,
+            validate: function(value) {
+                if (isNaN(value)) {
+                    return false;
+                } else {
+                    return true;
+                }
+            }
+        }
+
+    ]).then(function(answer) {
+
+        var query = "SELECT DepartmentName, StockQuantity, Price FROM products WHERE ?"
+        connection.query(query, { ItemID: answer.id }, function(err, res) {
+
+            if (res[0].StockQuantity >= answer.quantity) {
+
+                var dept = res[0].DepartmentName;
+                var adjustedQuantity = res[0].StockQuantity - answer.quantity;
+                var purchasePrice = (answer.quantity * res[0].Price).toFixed(2);
+
+                var query2 = " UPDATE products SET ? WHERE ?";
+                connection.query(query2, [{ StockQuantity: adjustedQuantity }, { ItemID: answer.id }],
+
+                    function(err, res) {
+
+                        if (err) throw err;
+                        console.log("Success! Your total is $".red.bold + purchasePrice.red.bold + "\nYour item(s) will be shipped to you in 3-5 business days.".bold);
+
+                    });
+
+
+
+                var query3 = "SELECT TotalSales FROM Departments WHERE ?"
+                connection.query(query3, { DepartmentName: dept }, function(err, data) {
+
+                    if (err) throw err
+
+                    var currentSales = data[0].TotalSales;
+                    var adjustedSales = currentSales + parseFloat(purchasePrice);
+
+
+
+
+
+                    var query4 = "UPDATE Departments SET ? WHERE ? "
+                    connection.query(query4, [{ TotalSales: adjustedSales }, { DepartmentName: dept }], function(err, data) {
+
+                        if (err) throw err
+                        start();
+
+
+                    })
+
+                })
+
+            } else {
+                console.log("Sorry, there are ".bold + res[0].StockQuantity + " units in stock for this product".bold);
+                console.log("\n-----------------------------------------\n");
+
+                start();
+
+            }
+
+            
+
+        })
+
+    })
+}
